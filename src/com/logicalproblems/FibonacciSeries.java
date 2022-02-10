e18806df7abdddf93296444cfe93efdb72b87318
karthikbenki <karthikmc007@gmail.com>
2022-02-10 08:54:36 +0530

    Refactored: code

--- src/com/logicalproblems/FibonacciSeries.java
+++ src/com/logicalproblems/FibonacciSeries.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class FibonacciSeries {
 
 	public static void fibonacciSeries(int range) {
-		int a = 0;
+		int a = 0;// 0,1,1,2,3,5,8
 		int b = 1;
 		int c = 0;
 		System.out.print(a + " " + b + " ");
 		while (true) {
-			c = a + b;
+			c = a + b;// c=2//c=3
 			if (c > range) {
 				break;
 			}
 			System.out.print(c + " ");
-			a = b;
-			b = c;
+			a = b;// a=1//a=1
+			b = c;// b=1//b=2
 
 		}
 
