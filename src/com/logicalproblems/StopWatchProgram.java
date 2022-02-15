2027c748b4ff72ddec62f642c7636298d83196c0
karthikbenki <karthikmc007@gmail.com>
2022-02-15 21:39:30 +0530

    Refactored: stop watch program logic

--- src/com/logicalproblems/StopWatchProgram.java
+++ src/com/logicalproblems/StopWatchProgram.java
@@ -12,28 +12,30 @@ d. O/P -> Print the elapsed time.
  */
 
 public class StopWatchProgram {
-	public static double startWatch() {
-		System.out.println("Enter 1 to start watch");
-		Scanner scanner = new Scanner(System.in);
-		double startWatch = scanner.nextDouble();
-		startWatch = System.currentTimeMillis();
-		return startWatch;
-
+    public static double startWatch() {
+	System.out.println("Enter 1 to start watch");
+	try (Scanner scanner = new Scanner(System.in)) {
+	    double startWatch = scanner.nextDouble();
+	    startWatch = System.currentTimeMillis();
+	    return startWatch;
 	}
 
-	public static double stopWatch() {
-		System.out.println("Enter 0 to stop watch");
-		Scanner scanner = new Scanner(System.in);
-		double stopWatch = scanner.nextDouble();
-		stopWatch = System.currentTimeMillis();
-		return stopWatch;
+    }
 
+    public static double stopWatch() {
+	System.out.println("Enter 0 to stop watch");
+	try (Scanner scanner = new Scanner(System.in)) {
+	    double stopWatch = scanner.nextDouble();
+	    stopWatch = System.currentTimeMillis();
+	    return stopWatch;
 	}
 
-	public static void main(String[] args) {
-		double timeInSec = Math.abs((startWatch() - stopWatch())) / 1000;
-		System.out.println("time in sec is " + timeInSec);
+    }
 
-	}
+    public static void main(String[] args) {
+	double timeInSec = Math.abs((startWatch() - stopWatch())) / 1000;
+	System.out.println("time in sec is " + timeInSec);
+
+    }
 
 }
